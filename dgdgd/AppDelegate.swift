92bf8b8d1c33a728d10baff825ffca52cfc08fd6
jiikko <n905i.1214@gmail.com>
2020-11-24 11:44:13 +0900

    hi

--- dgdgd/AppDelegate.swift
+++ dgdgd/AppDelegate.swift
@@ -14,6 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        DeployGateSDK
+            .sharedInstance()
+            .launchApplication(withAuthor: "USERNAME", key: "API_KEY")
         return true
     }
 
---
+++ dgdgd/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  dgdgd
+//
+//  Created by KOJI KAWAGUCHI on 2020/11/24.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <DeployGateSDK/DeployGateSDK.h>
+
+#endif /* Bridging_Header_h */
