da83100d126124970b6c8a2fae760ef4663f8564
jiikko <n905i.1214@gmail.com>
2020-11-24 11:25:25 +0900

    Initial Commit

---
+++ dgdgd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dgdgd
+//
+//  Created by KOJI KAWAGUCHI on 2020/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
